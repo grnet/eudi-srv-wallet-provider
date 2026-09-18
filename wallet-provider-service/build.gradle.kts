cd9045bcd6b79119eba769177f39b7d011665992
Despoina Ntolka <dntolka@admin.grnet.gr>
2026-09-18 15:11:16 +0300

    Remove broken in-app TLS attempt (ktor ssl block never compiled, provider runs from prebuilt image anyway)

--- wallet-provider-service/build.gradle.kts
+++ wallet-provider-service/build.gradle.kts
@@ -203,17 +203,3 @@ dependencyCheck {
 tasks.test {
     useJUnitPlatform()
 }
-
-ktor {
-    security {
-        ssl {
-            keyStore = keystore.jks
-            keyAlias = tls
-            keyStorePassword = password
-            privateKeyPassword = password
-            // trustStore = truststore.jks
-            // trustStorePassword = password
-            enabledProtocols = ["TLSv1.2", "TLSv1.3"]
-        }
-    }
-}
